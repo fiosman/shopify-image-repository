239095aeca7006d5c1dbfa41bb98e68b7b3c9967
Fares <fares.osman@outlook.com>
2021-05-07 23:19:39 -0400

    Start writing comments migration

---
+++ db/migrate/20210508031751_create_comments.rb
@@ -0,0 +1,15 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id, null: false
+      t.integer :image_id, null: false
+      t.text :body, null: false
+      t.integer :rating, null: false, default: 1
+
+      t.timestamps
+    end
+
+    add_index :comments, :user_id
+    add_index :comments, :image_id
+  end
+end
