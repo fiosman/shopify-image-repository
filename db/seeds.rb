57b73a399bbe6f68896ac6aea7f51d1b9545a4ed
Fares <fares.osman@outlook.com>
2021-05-11 00:20:46 -0400

    Set up seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demo_user = User.create(username: "Fares", password: "test12345")
+
+demo_image = Image.new(title: "Piggy", body: "Nice", user_id: User.last.id)
+
+file = File.open("app/assets/images/1902.jpg")
+
+demo_image.photo.attach(io: file, filename: "pig.jpg")
+
+demo_image.save!
